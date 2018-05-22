eacd92eadbeaa2f1cb498adf3ecfe1b249b1df2d
Kaja89 <kajaspiro@gmail.com>
2018-05-23 01:45:35 +0200

    first version front using bootstrap

---
+++ script.js
@@ -0,0 +1,25 @@
+
+// $(document).ready(function() {
+// 	console.log( "ready!" );
+// 	const apiRoot = 'http://localhost:8080/restLibrary/';
+// 	var availableBooks = {};
+
+// 	getAllBooks();
+
+// 	function getAllBooks(){
+// 		const requestUrl = apiRoot + 'getBooks';
+
+// 		$.ajax({
+//       	url: requestUrl,
+//       	method: 'GET',
+//       	contentType: "application/json",
+//       	// success: function(books) {
+//        //  books.forEach(books=> {
+//        //   availableBooks[book.id] = book;
+//        //  });
+
+//         //getAllAvailableBoards(handleDatatableRender, tasks);
+//       //}
+//     });
+// 	}
+// });
